cda512caed0aa9bcd614462024715fd73f29a5c9
kanz-z <kanzaaffa@gmail.com>
2024-11-08 17:38:37 +0700

    sigma

--- info1.js
+++ info1.js
@@ -2,4 +2,8 @@ const kali = (a, b) => a * b;
 console.log(kali(5, 10));
 
 const tambahTigaAngka = (a, b, c) => a + b + c;
-console.log(tambahTigaAngka(3, 4, 5))
+console.log(tambahTigaAngka(3, 4, 5));
+
+function sigma() {
+    console.log("Suttt, mewing dulu sana.");
+};
